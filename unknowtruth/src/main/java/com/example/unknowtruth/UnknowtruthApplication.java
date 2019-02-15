72c0f5546aa7598926f8ed955a5661b74a8ebb01
hjr <>
2019-02-15 11:36:56 +0800

    1

---
+++ unknowtruth/src/main/java/com/example/unknowtruth/UnknowtruthApplication.java
@@ -0,0 +1,14 @@
+package com.example.unknowtruth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnknowtruthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnknowtruthApplication.class, args);
+    }
+
+}
+
